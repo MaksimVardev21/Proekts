287f13fdca514731652d58cc7073695de887f9e5
IvailoPetrov06 <97381241+IvailoPetrov06@users.noreply.github.com>
2022-06-07 18:14:29 +0300

    util.js

--- util.js
+++
@@ -1,9 +0,0 @@
-import moment from 'moment';
-export function formatDate(dateString) {
- const parsed = moment(new Date(dateString));
- if(!parsed.isValid()) {
- return Date.now()
- }
-
- return parsed.format('DD/MM/YY HH:mm');
-}
