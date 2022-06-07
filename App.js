01311872f29a7838e814209d2c77e00b37f01036
ZlatomiraDimova06 <102371554+Yukkiiiiii@users.noreply.github.com>
2022-06-07 20:25:17 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import BusinessTripForm from './BusinessTripForm';
+import BusinessTripList from './BusinessTripList';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer
+    >
+      <Stack.Navigator initialRouteName='BusinessTripList'>
+      <Stack.Screen name="BusinessTripList" component={BusinessTripList} options={{title: 'BusinessTripList'}}/>
+      <Stack.Screen name="BusinessTripForm" component={BusinessTripForm} options={{title: 'BusinessTripForm'}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
