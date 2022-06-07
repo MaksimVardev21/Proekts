0087847a0e1285cc3c61f1fabb810a60e95ea95c
Kaloyan Ivanov <cdevyt@gmail.com>
2022-06-07 18:56:50 +0300

    Delete function

--- BusinessTripService.js
+++ BusinessTripService.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import moment from 'moment';
 import uuid from 'uuid';
 
@@ -50,25 +49,3 @@ export function Delete(id) {
         console.log(body);
     })
 }
-=======
-import moment from 'moment';
-import uuid from 'uuid';
-
-const BASE_URL = 'https://proekts-953e.restdb.io/rest/title';
-
-export function getEvents() {
-    return fetch(BASE_URL,
-    {
-    method: 'GET',
-    headers: {
-    'content-type': 'application/json',
-    'x-apikey': '597d59cd454c830413d1734d7a67201452bff'
-    },
-    })
-    .then(response => response.json())
-    .then(events => events.map(item => ({
-    ...item,
-    date: moment(item.date, "DD/MM/YYYY HH:mm").toDate()
-    })))
-   }
->>>>>>> 30de98fc117cc840976dec417958adf05c685071
