13a9844005dfe72a31742a17d174f1a4194384bb
IvailoPetrov06 <scriptewepro4@gmail.com>
2022-06-21 21:47:57 +0300

    done BusinessTripList

---
+++ BusinessTripForm.js
@@ -0,0 +1,4 @@
+import React, { Component } from "react";
+import { View, Button, TextInput, StyleSheet } from "react-native";
+import { formatDate } from './util';
+import { Add } from "./BusinessTripService";
--- BusinessTripService.js
+++ BusinessTripService.js
@@ -1,36 +1,79 @@
 import moment from 'moment';
 import uuid from 'uuid';
 
-const BASE_URL = '';
+const BASE_URL = 'https://proekts-953e.restdb.io/rest/title';
+const apikey = '597d59cd454c830413d1734d7a67201452bff';
 
-export function getEvents() {
-    return fetch(BASE_URL,
-    {
-    method: 'GET',
-    headers: {
-    'content-type': 'application',
-    'x-apikey': 'api key'
-    },
-    })
-    .then(response => response.json())
-    .then(events => events.map(item => ({
-    ...item,
-    date: moment(item.date, "DD/MM/YYYY HH:mm").toDate()
-    })))
-   }
-   export function addEvent({title, date}) {
+const request = require("request");
+export function Get() {
+    var options = {
+        method: 'GET',
+        url: BASE_URL,
+        headers:
+            { 'x-apikey': apikey }
+    };
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+    });
+}
+export function Getbyid(id) {
+    var options = {
+        method: 'GET',
+        url: `${BASE_URL}/${id}`,
+        headers:
+            { 'x-apikey': apikey }
+    };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+    });
+}
+export function Add(budget, name, location, startDate, description, days) {
     return fetch(BASE_URL, {
-    method: 'POST',
-    headers: {
-    'content-type': 'application',
-    'x-apikey': ' '
-    },
-    body: JSON.stringify({
-    title,
-    date,
-    id: uuid()
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'x-apikey': '597d59cd454c830413d1734d7a67201452bff'
+        },
+        body: JSON.stringify({
+            budget,
+            name,
+            location,
+            startDate,
+            description,
+            days,
+        })
     })
+        .then(result => result.json())
+        .catch(error => console.log(error))
+}
+
+export function Delete(id) {
+    const options = {
+        method: "DELETE",
+        url: `${BASE_URL}/${id}`,
+        headers: {
+            'x-apikey': apikey,
+            'content-type': 'application/json'
+        }
+    };
+    request(BASE_URL, options, function (error, response, body) {
+        if (error) throw new Error(error);
     })
-    .then(result => result.json())
-    .catch(error => console.log(error))
-   }
+}
+export function Update(id, budget, name, location, startDate, description, days) {
+    const options = {
+        method: "PUT",
+        url: `${BASE_URL}/${id}`,
+        headers: {
+            'x-apikey': apikey,
+            'content-type': 'application/json'
+        },
+        body: {
+            "budget": budget, "name": name, "location": location, "startDate": startDate, "description": description, "days": days
+        }
+    };
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+    })
+}
