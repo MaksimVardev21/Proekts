56579ca142861466967ff54e41ab4c3bc685693c
IvailoPetrov06 <97381241+IvailoPetrov06@users.noreply.github.com>
2022-06-21 21:56:33 +0300

    Delete BusinessTripForm.js

--- BusinessTripForm.js
+++
@@ -1,4 +0,0 @@
-import React, { Component } from "react";
-import { View, Button, TextInput, StyleSheet } from "react-native";
-import { formatDate } from './util';
-import { Add } from "./BusinessTripService";
